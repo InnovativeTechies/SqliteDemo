aee8750030acf79249588160dbebbc9af8a24e30
Rakesh Kumar <Rakesh Kumar@DESKTOP-GIRT8F7>
2017-02-10 13:41:36 +0530

    Add project files.

---
+++ SqliteDemo/SqliteDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SqliteDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GetSettings(object sender, EventArgs e)
+        {
+            SettingValue.Text = SqliteDemo.Helpers.Settings.GeneralSettings;
+        }
+
+        private void SetSettings(object sender, EventArgs e)
+        {
+            SqliteDemo.Helpers.Settings.GeneralSettings = SetText.Text;
+        }
+    }
+}
